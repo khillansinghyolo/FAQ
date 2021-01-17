cae39cc9ba960ceb0e213a93187808e74f4a35a0
khillansinghyolo <khillanverma@gmail.com>
2021-01-17 19:47:05 +0530

    Odd even number

---
+++ src/Frequently_Asked_Java/com/Odd_Even_Number.java
@@ -0,0 +1,21 @@
+package Frequently_Asked_Java.com;
+
+public class Odd_Even_Number {
+
+	public static void main(String[] args) {
+		int num=11;
+		int even=0,odd =0;
+		odd=even=num;
+		if(num%2==0)
+		{
+			System.out.println("Number is even :"+even);
+			
+		}
+		else
+		{
+			System.out.println("Number is odd :"+odd);
+		}
+
+	}
+
+}
