bc167a04689152dee51fb1c53d03d7173c3eedad
khillansinghyolo <khillanverma@gmail.com>
2021-01-17 18:43:35 +0530

    commit 3

--- src/Frequently_Asked_Java/com/ReverseString.java
+++ src/Frequently_Asked_Java/com/ReverseString.java
@@ -1,10 +1,32 @@
 package Frequently_Asked_Java.com;
 
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class ReverseString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//Scanner s=new Scanner(System.in);
+		System.out.println("Please Enter String :");
+		//String str=s.next();
+		//String rev="";
+		/*
+		 * int len=str.length(); for(int i=len-1;i>=0;i--) { rev=rev+str.charAt(i); }
+		 * System.out.println(rev);
+		 */
+		String  str="ram singh khillan";
+		StringBuffer sb= new StringBuffer(String.valueOf(str));
+		StringBuffer rev=sb.reverse();
+		
+		StringBuffer sssss=rev.reverse();
+		
+		System.out.println(sssss.toString());
+		
+		/*
+		 * int l= s.length; String sss=" "; for(int i=l-1;i>=0;i--) { sss=sss+s[i]+" ";
+		 * } System.out.println("Revrse string :"+sss);
+		 */
 	}
 
 }
---
+++ src/Frequently_Asked_Java/com/SetOfStringReverse.java
@@ -0,0 +1,9 @@
+package Frequently_Asked_Java.com;
+
+public class SetOfStringReverse {
+
+	public static void main(String[] args) {
+
+	}
+
+}
